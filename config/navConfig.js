c4e7a4e8aa7a95e09a1ba5e59ca0be3ad2b8b7de
PiotrKulpa <info.kulpa@gmail.com>
2019-03-03 00:21:58 +0100

    feat: menu styling

--- config/navConfig.js
+++ config/navConfig.js
@@ -11,6 +11,34 @@ const RouteConfigs = {
 
 const TabNavigatorConfig = {
   tabBarPosition: 'top',
+  initialRouteName: 'Timer',
+  tabBarOptions: {
+    style: {
+      backgroundColor: 'transparent',
+      borderWidth: 0,
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 40,
+      height: 50,
+      zIndex: 99,
+      elevation: 0,       //remove shadow on Android
+      shadowOpacity: 0,
+    },
+    inactiveTintColor: 'rgba(66,93,155,0.5)',
+    labelStyle: {
+      fontSize: 22,
+    },
+    tabStyle: {
+      borderWidth: 0,
+      shadowColor: 'transparent'
+    },
+    indicatorStyle: {
+      backgroundColor: 'transparent',
+      borderWidth: 0
+    },
+    showLabel: true
+  }
 };
 
 export const TabNavigator = createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig);
