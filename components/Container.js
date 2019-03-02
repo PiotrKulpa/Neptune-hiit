8527381888713f370f3e697a3af00de78915050e
PiotrKulpa <info.kulpa@gmail.com>
2019-03-02 16:42:34 +0100

    feat: todo

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+//TODO: add background, logo, images, and basic layout
 export default class Container extends Component {
   render() {
     return (
