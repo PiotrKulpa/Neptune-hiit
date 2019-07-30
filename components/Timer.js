1ad9fa7233fd7cc3f072053fb5322485590d1681
PiotrKulpa <info.kulpa@gmail.com>
2019-07-30 20:23:13 +0200

    add sounds method

--- components/Timer.js
+++ components/Timer.js
@@ -21,13 +21,9 @@ import Done from './Done';
 const db = SQLite.openDatabase('results');
 
 class Timer extends Component {
-  constructor(props)
-    {
-      super(props);
-
-      this.soundObject = new Audio.Sound();
-
-    }
+  constructor(props) {
+    super(props);
+  }
 
   state = {
     n: 0,
@@ -39,12 +35,35 @@ class Timer extends Component {
     isTimerRun: false,
   }
 
+  onLoadSound = async () => {
+
+    const soundObject = new Expo.Audio.Sound();
+      try {
+        await soundObject.loadAsync(require('../assets/sounds/beep.mp3'));
+      { shouldPlay: true }
+        this.audioPlayer1  = soundObject;
+          this.audioPlayer1.playAsync();
+          this.audioPlayer1.setPositionAsync(0);
+          this.audioPlayer1.setRateAsync(3, false);
+       // Your sound is playing!
+      } catch (error) {
+      // An error occurred!
+      } 
+  }
+
+  async componentWillMount(){
+    
+    try{
+      await Expo.Audio.setIsEnabledAsync(true);
+    } catch(error) {console.log(error);}
+  }
+
   componentDidMount() {
 
     //load custom fonts
     Font.loadAsync({
       'sans-pro-light': require('../assets/fonts/SourceSansPro-Light.ttf'),
-    }).then(() => this.setState({fontsLoaded: true}));
+    }).then(() => this.setState({ fontsLoaded: true }));
 
     // create sql table results if not exist
     db.transaction(tx => {
@@ -53,19 +72,12 @@ class Timer extends Component {
       );
     });
 
-    this.soundObject.loadAsync(require('../assets/sounds/beep.mp3')).then(sound => {
-      console.log(sound);
-      this.soundObject.playAsync();
-    });
-  }
-
-  onLoadSound= () => {
-   this.soundObject.playAsync().then(() => console.log('uruchomils ie dzwiek')); 
   }
 
   
+
+
   onStart = () => {
-    //this.soundObject.playAsync();
     this.onLoadSound();
     // reset timer
     this.state.timerAnim.resetAnimation();
@@ -74,18 +86,20 @@ class Timer extends Component {
     //reset value to 100
     this.state.timerAnim._value = 100;
     // add listener
-    this.state.timerAnim.addListener(({value}) => {
+    this.state.timerAnim.addListener(({ value }) => {
       this._value = value;
       // round value to integral
-      this.setState({counter: parseInt(value)});
+      this.setState({ counter: parseInt(value) });
 
       // reset animation
-      if(value === 0) {
+      if (value === 0) {
         this.state.timerAnim.resetAnimation();
+        this.onLoadSound();
 
         // methods after all sets finished
-        if(n === this.props.intervals.length -1) {
+        if (n === this.props.intervals.length - 1) {
           Vibration.vibrate([0, 100, 200, 100, 200, 100]);
+          this.onLoadSound();
           this.state.timerAnim.removeAllListeners();
           n = 0;
           this.state.timerAnim._value = 0;
@@ -98,10 +112,10 @@ class Timer extends Component {
           return;
         };
         n++;
-        this.setState({duration: this.props.intervals[n]}, this.onAnimate(n));
-      }; 
+        this.setState({ duration: this.props.intervals[n] }, this.onAnimate(n));
+      };
     });
-    
+
     this.setState({
       duration: this.props.intervals[0],
       wellDone: false,
@@ -121,24 +135,24 @@ class Timer extends Component {
   }
 
   onAnimate(n) {
-    const {intervals} = this.props;
+    const { intervals } = this.props;
     Vibration.vibrate(500);
-    
-      Animated.timing(                 
-        this.state.timerAnim,            
-        {
-          toValue: 0,
-          duration: this.props.intervals[n] * 1000,
-          easing: Easing.linear,
-          useNativeDriver: false,
-          delay: 1000
-        }
-      ).start()
-    
-   }
+
+    Animated.timing(
+      this.state.timerAnim,
+      {
+        toValue: 0,
+        duration: this.props.intervals[n] * 1000,
+        easing: Easing.linear,
+        useNativeDriver: false,
+        delay: 1000
+      }
+    ).start()
+
+  }
 
   onFocus = () => {
-    if(!this.state.isTimerRun) {
+    if (!this.state.isTimerRun) {
       this.setState({
         duration: this.props.intervals[0],
       });
@@ -148,71 +162,71 @@ class Timer extends Component {
   writeToDB() {
     // get all data to write to DB
     let hiitDate = new Date().toLocaleDateString() + ' - ' + new Date().toLocaleTimeString(),
-        hiitSets = this.props.intervals.length / 2,
-        hiitTime = this.props.intervals.reduce((partial_sum, a) => partial_sum + a,0);
+      hiitSets = this.props.intervals.length / 2,
+      hiitTime = this.props.intervals.reduce((partial_sum, a) => partial_sum + a, 0);
     // write data to DB
     db.transaction(tx => {
       tx.executeSql('insert into intervals (date, sets, value) values (?, ?, ?)', [hiitDate, hiitSets, hiitTime]);
     });
-    
+
   }
 
   render() {
     let { timerAnim, duration } = this.state;
     // calculate seconds
-    let timerTime = parseInt(timerAnim._value * duration / 100); 
+    let timerTime = parseInt(timerAnim._value * duration / 100);
     return (
-        this.state.fontsLoaded ? (
-      <View>
+      this.state.fontsLoaded ? (
+        <View>
           <NavigationEvents
             onWillFocus={this.onFocus}
           />
-        <View style={styles.ring}>
           <View style={styles.ring}>
-            <Animated.View 
-              style={{
-                ...this.props.style,
-              }}
-            >
-              <ProgressCircle
+            <View style={styles.ring}>
+              <Animated.View
+                style={{
+                  ...this.props.style,
+                }}
+              >
+                <ProgressCircle
                   percent={timerAnim._value}
                   radius={120}
                   borderWidth={8}
                   color="#35e3ee"
                   shadowColor="#1d305b"
                   bgColor="#304876"
-              >
-                  <View style={!this.state.wellDone ? {display: 'flex'} : {display: 'none'}}>
-                    <Text style={{ fontSize: 68, color: "#35e3ee", textAlign: 'center'}}>{timerTime}</Text>
+                >
+                  <View style={!this.state.wellDone ? { display: 'flex' } : { display: 'none' }}>
+                    <Text style={{ fontSize: 68, color: "#35e3ee", textAlign: 'center' }}>{timerTime}</Text>
                     <Text style={{ fontSize: 22, color: "white", }}>{'to Complete'}</Text>
                   </View>
-                  <View style={this.state.wellDone ? {display: 'flex'} : {display: 'none'}}>
+                  <View style={this.state.wellDone ? { display: 'flex' } : { display: 'none' }}>
                     {this.state.wellDone && <Done />}
-                    
+
                     <Text style={{ fontSize: 22, color: "white", }}>{'Well Done!'}</Text>
                   </View>
-              </ProgressCircle>
-            </Animated.View>
+                </ProgressCircle>
+              </Animated.View>
+            </View>
           </View>
-        </View>
 
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-          <MainButton 
-            onPress={this.onStart}
-          >
-            <Text style={{ fontFamily: 'sans-pro-light' }}>START</Text>
-          </MainButton>
-          <MainButton
-            onPress={this.onReset}
-          >
-            <Text style={{ fontFamily: 'sans-pro-light' }}>RESET</Text>
-          </MainButton>
-        </View>
-        <View><Text>{this.state.msg}</Text></View>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <MainButton
+              onPress={this.onStart}
+            >
+              <Text style={{ fontFamily: 'sans-pro-light' }}>START</Text>
+            </MainButton>
+            <MainButton
+              onPress={this.onReset}
+            >
+              <Text style={{ fontFamily: 'sans-pro-light' }}>RESET</Text>
+            </MainButton>
+          </View>
+          <View><Text>{this.state.msg}</Text></View>
 
-      </View>
-        ) : null 
-      
+        </View>
+      ) : null
+
     )
   }
 }
